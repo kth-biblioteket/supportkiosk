43bd6b73d277d29a8ad70028653f6b4260fa9622
Thomas Lind <thomas.lind@tlcgolit.se>
2026-09-11 09:36:58 +0200

    matomo url

--- src/supportkiosk/script/custom.js
+++ src/supportkiosk/script/custom.js
@@ -4,7 +4,7 @@ var _paq = window._paq = window._paq || [];
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function() {
-        var u="https://analytics.sys.kth.se/";
+        var u="https://analytics-stats.sys.kth.se/";
         _paq.push(['setTrackerUrl', u+'matomo.php']);
         _paq.push(['setSiteId', '7']);
         var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
